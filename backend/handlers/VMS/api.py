8da34d8ede81e84260a9c1c77e856f2d51913640
Jerric1801 <Jerric1801@gmail.com>
2024-03-02 16:13:18 +0800

    VMS api

--- backend/handlers/VMS/api.py
+++ backend/handlers/VMS/api.py
@@ -1,12 +1,64 @@
 import os
 from dotenv import load_dotenv
 import requests
+import json
+from ..utils.geocode import geocode
+import asyncio
 
-load_dotenv()
+class api:
+    url = "http://datamall2.mytransport.sg/ltaodataservice/"
 
-key = os.getenv('api_key')
+    def __init__(self):
+        load_dotenv()
+        self.key = os.getenv('api_key')
+        self.headers  = {
+            'Accept': 'application/json',          
+            'AccountKey': self.key             
+        }
 
-url = "http://datamall2.mytransport.sg/ltaodataservice/VMS"
+    def fetch_api(self, domain):
+        try:
+            response = requests.get(self.url + domain, headers=self.headers)
+            response.raise_for_status() 
+            return json.loads(response.content)
+        
+        except requests.exceptions.RequestException as e:
+            print(f"Error fetching data: {e}")
+            return None 
+        
+    def process(self, values):
+        geolocator = geocode()
+        for item in values:
+            type = item["Type"]
+            lat = item["Latitude"]
+            long = item["Longitude"]
+            msg = item["Message"]
 
-response = requests.post(url, headers=key)
-print(response)
+            address = asyncio.run(geolocator.reverse(f"{lat}, {long}"))
+
+            print(f"type {type} accident at {address}")
+
+    def incidents(self):
+        domain = "TrafficIncidents"
+        content = self.fetch_api(domain)
+        if content["value"]:
+            values = content["value"]
+            self.process(values)
+        else:
+            return None
+
+    def warnings(self):
+        domain = "VMS"
+        content = self.fetch_api(domain)
+        if content["value"]:
+            values = content["value"]
+            self.process(values)
+        else:
+            return None
+
+
+if __name__ == "__main__":
+    obj = api()
+    obj.warnings
+
+
